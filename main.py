f95f71c2f3bae63a381e4f0013a0eafb57095692
gmnr <guido.minieri@gmail.com>
2019-07-08 23:32:19 +0200

    implemented main funcionality

--- main.py
+++ main.py
@@ -31,4 +31,26 @@ for item in soup.find_all('td'):
         else:
             towns.append(item.string)
 
-print(towns)
+# trim garbage in the html parsing
+towns = towns[:-11]
+
+# variable storage
+double = []
+triple = []
+quadruple = []
+
+# iterate through the towns to separate the ones that have 2 or more names
+for town in towns:
+    if len(town.split()) == 2:
+        double.append(town)
+    if len(town.split()) == 3:
+        triple.append(town)
+    if len(town.split()) == 4:
+        quadruple.append(town)
+    if len(town.split()) == 5:
+        quintuple.append(town)
+
+# print results
+print(len(double))
+print(len(triple))
+print(len(quadruple))
