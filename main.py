48f205aaa5b4dc4f76719f45d5c1110aacbb8f8f
gmnr <guido.minieri@gmail.com>
2019-07-08 23:23:29 +0200

    get all towns in the province of Como in a list

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Docstring
+Catalogue all towns in the province of Como, based on the # of words in the name
 """
 
 __author__ = 'Guido Minieri'
@@ -12,3 +12,23 @@ __version__ = 'x.x.x'
 
 # imports
 import requests
+import bs4
+
+
+# define the url
+url = "http://www.comuni-italiani.it/013/lista.html"
+
+res = requests.get(url)
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+towns = []
+for item in soup.find_all('td'):
+    if item.string == None:
+        continue
+    else:
+        if item.string[0] in "0123456789":
+            continue
+        else:
+            towns.append(item.string)
+
+print(towns)
